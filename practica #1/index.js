50734482ae6e7761436deb4406d1d17edc9e12b4
melissaraujo00 <140029893+melissaraujo00@users.noreply.github.com>
2023-08-11 11:58:01 -0600

    Add files via upload

---
+++ practica #1/index.js
@@ -0,0 +1,32 @@
+//Ejercicio 1
+// let edad  = prompt("Introduzca su edad:")
+
+// if (edad > 18 && edad <= 100){
+//     alert("El usuario es mayor de edad")
+// }else if( edad < 18){
+//     alert("El usuario es menor de edad")
+// }else if(edad == 18){
+//     alert("El usuario tiene 18")
+// }else if(edad > 100){
+//     alert("El usuario es centario")
+// }else if(edad <= 0){
+//     alert("Edad invalidad")
+// }
+
+//Ejercicio 2
+// let i=1;
+// let suma = 0;
+// while (i<= 100) {
+//     suma += i;
+//     i++;
+// }
+
+// alert(suma)
+
+//Ejercicio 3
+let num = prompt("Escriba un numero: ");
+
+for ( i = 1; i <= 10; i++) {
+    alert(num + " x " + i + " = " + (num*i)) 
+
+}
